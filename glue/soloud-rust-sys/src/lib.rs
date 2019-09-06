b93f295117592dd3fe88d17a585f042a138f31cc
Mason Remaley <MasonRemaley@users.noreply.github.com>
2019-09-06 14:21:41 -0700

    Removes dummy tests

--- glue/soloud-rust-sys/src/lib.rs
+++ glue/soloud-rust-sys/src/lib.rs
@@ -139,11 +139,3 @@ extern "C" {
         aTakeOwnership: c_int, /* = true */
     ) -> c_int;
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
